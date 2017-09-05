a49cac9883cad851915367a027f3c876e97f07eb
Iurii Kozuliak <i.kozuliak@gmail.com>
2017-09-05 13:44:21 +0300

    Add general Card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,16 @@
+import React     from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './Card.css';
+
+const Card = ({ children }) => (
+  <div className={styles.Card}>
+    { children }
+  </div>
+);
+
+Card.propTypes = {
+  children: PropTypes.element.isRequired
+};
+
+export default Card;
---
+++ src/components/Card/__tests__/Card.js
@@ -0,0 +1,19 @@
+import React    from 'react';
+import renderer from 'react-test-renderer';
+import Card     from '../Card';
+
+test('Renders Card component', () => {
+  const component = renderer.create(
+    <Card />
+  ).toJSON();
+  expect(component).toMatchSnapshot();
+});
+
+test('Renders Card component with passed children', () => {
+  const component = renderer.create(
+    <Card>
+      <h2>Card Title</h2>
+    </Card>
+  ).toJSON();
+  expect(component).toMatchSnapshot();
+});
